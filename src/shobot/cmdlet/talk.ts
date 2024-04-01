6044742a11c4416c13eb254c84cdac52f7a2e0f3
suyo_haru <suyo_haru@outlook.jp>
2024-04-02 02:57:07 +0900

    [Chore: talk] lint

--- src/shobot/cmdlet/talk.ts
+++ src/shobot/cmdlet/talk.ts
@@ -19,15 +19,15 @@ export default class TalkCmdlet extends ShobotCmdletBase {
   async run(...args: string[]) {
     const anthropic = new Anthropic({
       apiKey: CLAUDE.API_KEY,
-      fetch: globalThis.fetch
+      fetch: globalThis.fetch,
     });
     console.log(args);
     const now = new Date().toLocaleString("ja-JP", {
-      dateStyle: 'full',
-      timeZone: 'JST',
+      dateStyle: "full",
+      timeZone: "JST",
     }) + " " + new Date().toLocaleTimeString("ja-JP", {
       hour12: true,
-      timeZone: 'JST',
+      timeZone: "JST",
     });
     console.log(now);
     let tempString = "";
@@ -59,45 +59,57 @@ In <Emotion> tag, you mush choose only from these emotions: [Normal, Happy, Angr
 
 Example:
 <Shobot><Emotion>Happy</Emotion>おはようございます、マスター!<Emotion>Smile</Emotion>今日も一日がんばりましょう。<Emotion>Thinking</Emotion>今日はどんな予定がありますか？</Shobot>`,
-      messages: [{role: "user", content: [{type: "text", text: `<Date>${now}</Date><User>${args.join(" ")}</User>`}]}],
+      messages: [{
+        role: "user",
+        content: [{
+          type: "text",
+          text: `<Date>${now}</Date><User>${args.join(" ")}</User>`,
+        }],
+      }],
       model: "claude-3-opus-20240229",
       stop_sequences: ["</Shobot>"],
     }).on("text", (textDelta: string, textSnapshot: string) => {
       tempString += textDelta;
-      if(convFlag && !emoFlag) {
+      if (convFlag && !emoFlag) {
         sendString += textDelta;
       }
-      if(tempString.includes("<Shobot>")) {
-        tempString = tempString.replace("<Shobot>", "")
+      if (tempString.includes("<Shobot>")) {
+        tempString = tempString.replace("<Shobot>", "");
         convFlag = true;
-      } else if(tempString.includes("</Shobot>")) {
+      } else if (tempString.includes("</Shobot>")) {
         sendString = sendString.replace("</Shobot>", "");
-        if(sendString.length > 2) {
+        if (sendString.length > 2) {
           console.log(sendString.replace(/^[\n\r]*|[\n\r]*$/g, ""));
-          this.controller.enqueue({ type: "text", text: sendString.replace(/^[\n\r]*|[\n\r]*$/g, "").trim() });
-        };
+          this.controller.enqueue({
+            type: "text",
+            text: sendString.replace(/^[\n\r]*|[\n\r]*$/g, "").trim(),
+          });
+        }
         sendString = "";
       }
-      if(tempString.includes("<Emotion>")) {
+      if (tempString.includes("<Emotion>")) {
         tempString = tempString.replace("<Emotion>", "");
         sendString = sendString.replace("<Emotion>", "");
-        if(sendString.length > 2) { 
+        if (sendString.length > 2) {
           console.log(sendString.replace(/^[\n\r]*|[\n\r]*$/g, "").trim());
-          this.controller.enqueue({ type: "text", text: sendString.replace(/^[\n\r]*|[\n\r]*$/g, "").trim() });
+          this.controller.enqueue({
+            type: "text",
+            text: sendString.replace(/^[\n\r]*|[\n\r]*$/g, "").trim(),
+          });
           this.controller.enqueue({ type: "push" });
-        };
+        }
         tempString = "";
         sendString = "";
         emoFlag = true;
       }
-      if(tempString.includes("</Emotion>")) {
+      if (tempString.includes("</Emotion>")) {
         tempString = tempString.replace("</Emotion>", "");
-        sendString = sendString.replace("</Emotion>", "");   
+        sendString = sendString.replace("</Emotion>", "");
         emoFlag = false;
       }
     });
     await response.done();
-    console.log("完了")
+    console.log("完了");
     return;
   }
 }
